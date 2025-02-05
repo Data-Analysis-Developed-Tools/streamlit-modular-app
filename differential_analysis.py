6dcaae761cfd098d01f3565c983d505f60610d1c
Franco Rosso <169049117+FrancoR72@users.noreply.github.com>
2025-02-05 22:42:39 +0100

    Update differential_analysis.py

--- differential_analysis.py
+++ differential_analysis.py
@@ -2,6 +2,14 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+from volcano_plot import mostra_volcano_plot  # Ora dovrebbe funzionare
+
+
 from components.data_loader import carica_dati  # Importa la funzione di caricamento dati
 from volcano_plot_app import mostra_volcano_plot  # Importa la funzione per il Volcano Plot
 from table_display import mostra_tabella  # Importa la funzione per mostrare la tabella
