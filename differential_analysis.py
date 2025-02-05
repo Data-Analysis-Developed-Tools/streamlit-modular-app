fca18f6d5cce3a567ae6bb09c8792f793a20d65d
Franco Rosso <169049117+FrancoR72@users.noreply.github.com>
2025-02-05 21:46:49 +0100

    Update differential_analysis.py

--- differential_analysis.py
+++ differential_analysis.py
@@ -3,7 +3,6 @@ import os
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))  # Aggiunge la directory corrente ai percorsi di ricerca
 
-from table_app import mostra_tabella
 import streamlit as st
 from volcano_plot_app import mostra_volcano_plot
 from table_app import mostra_tabella
